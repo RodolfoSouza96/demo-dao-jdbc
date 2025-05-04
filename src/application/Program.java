8ae83330cfe8e79faef4b024f53d7b8f0c667ade
Rodolfo Souza <rodolfo.ls1996@outlook.com>
2025-05-04 19:56:44 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,11 +39,18 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println();
+		/*System.out.println();
 		System.out.println("=== TEST 4: Seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println("Inserted! New id = " + newSeller.getId());*/
+		
+		System.out.println();
+		System.out.println("=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update complete");
 	}
 	
 }
